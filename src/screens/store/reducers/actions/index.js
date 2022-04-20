5a23431622855fabaed81b244ff9b07d8f0ef0ac
yarami2k18 <yarami2k18@gmail.com>
2022-04-20 00:12:19 -0300

    implementacion del redux-thunk

--- src/screens/store/reducers/actions/index.js
+++ src/screens/store/reducers/actions/index.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import categoryReducer from '../../reducers/category.reducers';
 import productReducer from '../../reducers/product.reducer';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     categories: categoryReducer,
     products: productReducer,
 });
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, applyMiddleware(thunk));
---
+++ utils/database.js
@@ -0,0 +1,1 @@
+export const URL_API = 'https://breadshop-c0781-default-rtdb.firebaseio.com';
